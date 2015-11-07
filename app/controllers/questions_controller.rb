94e9dda8c2f3fe14220ee095e80afce63622e35c
dany-kun <dans44684@hotmail.com>
2015-11-07 17:26:01 +0100

    add question creation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,17 @@ class QuestionsController < ApplicationController
   def show
     question_id = params[:id]
     @question = Question.find(question_id)
-    @answers = Answer.joins(:question).where(questions: {id: question_id})
+    @answers = @question.answers
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    question = params[:question]
+    @question = Question.create(description: question[:description])
+    redirect_to(@question)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
 
   # Just show for the time being...
-  resources :questions, only: [:show]
+  resources :questions, only: [:show, :new, :create]
 
 end
