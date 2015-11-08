b67a6d115f1f0b92a6edd142c343d71b6429617a
Denis <endenis@gmail.com>
2015-11-09 00:25:51 +0100

    Lolmerge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,11 @@
 class QuestionsController < ApplicationController
 
+  def index
+    raw_questions = Question.order created_at: :desc
+    questions_per_page = paginate raw_questions, per_page: 10
+    @questions = questions_per_page
+  end
+
   def show
     question_id = params[:id]
     @question = retrieve_question question_id
